eae2da2c62c0e23b241ae92977797f24ac434d34
= <=>
2023-05-06 20:19:09 +0800

    21

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -19,35 +19,15 @@ def ridge(data):
 def lasso(data):
     
     X,y=read_data()
-    alpha = 1e-12
-    learning_rate = 1e-12
-    
-#     # 对数据进行归一化
-#     max_vals = np.max(X, axis=0)
-#     min_vals = np.min(X, axis=0)
-#     X = (X - min_vals) / (max_vals - min_vals)
-
-    # 初始化模型参数
-    theta = np.zeros(6)
-
-    # 定义L1正则化项的梯度
-    def l1(theta, alpha):
-        return alpha * np.sign(theta)
-
-    # 进行梯度下降迭代
-    for i in range(800):
-        # 计算模型预测值
-        y_pred = X @ theta
-
-        # 计算损失函数和L1正则化项
-        loss = np.mean((y_pred - y)**2) + alpha * np.sum(np.abs(theta))
-
-        # 计算梯度并更新模型参数
-        grad = (np.matmul(X.T, (np.matmul(X, theta) - y))) + l1(theta,alpha)
-        theta = theta - learning_rate * grad
-        if np.linalg.norm(grad)<1e-5:
+    m, n = X.shape
+    weight = np.zeros(n)
+    max_iterations = 100000
+    for i in range(max_iterations):
+        grad = (np.matmul(X.T, (np.matmul(X, weight) - y))) + 1e-12 * np.sign(weight)
+        weight = weight - 1e-12 * grad
+        if np.linalg.norm(grad) < 0.0001:
             break
-    return data @ theta
+    return weight @ data
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
